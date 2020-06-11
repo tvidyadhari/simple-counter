e5019953970ebb681f1203fa4c960208d9637f12
tvidyadhari <tvidyadhari22@gmail.com>
2020-06-11 16:56:09 +0530

    Updates

---
+++ precache-manifest.ddf1f7fbedcafb8828dc90e89e5d0c50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f904b4186a6fc2cec07207b43ee48ab",
+    "url": "/tvidyadhari/simple-counter/index.html"
+  },
+  {
+    "revision": "73bac39116459d90c9d3",
+    "url": "/tvidyadhari/simple-counter/static/css/main.7a3ecdf1.chunk.css"
+  },
+  {
+    "revision": "340be35330c24351fde1",
+    "url": "/tvidyadhari/simple-counter/static/js/2.ee3666a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tvidyadhari/simple-counter/static/js/2.ee3666a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73bac39116459d90c9d3",
+    "url": "/tvidyadhari/simple-counter/static/js/main.26e81004.chunk.js"
+  },
+  {
+    "revision": "0b7bfe685d90c7a72c17",
+    "url": "/tvidyadhari/simple-counter/static/js/runtime-main.342070dc.js"
+  }
+]);
